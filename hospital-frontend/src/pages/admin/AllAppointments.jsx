635ff0b9e17398eb15ad97ba2929f6048e34692e
ishandas994-cloud <ishandas994@gmail.com>
2026-05-30 09:17:58 +0530

    new code of import

--- hospital-frontend/src/pages/admin/AllAppointments.jsx
+++ hospital-frontend/src/pages/admin/AllAppointments.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useEffect, useState } from 'react'
 import { getAllAppointmentsAPI } from '../../api/adminAPI.js'
 import Navbar from '../../components/Navbar.jsx'
