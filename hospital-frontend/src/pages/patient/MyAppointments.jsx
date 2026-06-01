8ad061ffb377aaec1d4da6694a379ee0184a6abf
ishandas994-cloud <ishandas994@gmail.com>
2026-06-01 11:21:27 +0530

    new codes

--- hospital-frontend/src/pages/patient/MyAppointments.jsx
+++ hospital-frontend/src/pages/patient/MyAppointments.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useEffect, useState } from 'react'
 import { getMyAppointmentsAPI, cancelAppointmentAPI } from '../../api/appointmentAPI.js'
 import { createReviewAPI } from '../../api/patientAPI.js'
